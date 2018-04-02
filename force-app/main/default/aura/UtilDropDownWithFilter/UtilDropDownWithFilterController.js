473dd77810c21679b906369ea6569c13fa6935ae
Jacky Zhao <liang.zhao83@gmail.com>
2018-04-02 00:15:09 -0700

    normal dropdown function ready

--- force-app/main/default/aura/UtilDropDownWithFilter/UtilDropDownWithFilterController.js
+++ force-app/main/default/aura/UtilDropDownWithFilter/UtilDropDownWithFilterController.js
@@ -1,12 +1,25 @@
 ({
+    toggle : function(component, event, helper) {
+        var target = event.currentTarget;
+        if(target.getAttribute('aria-expanded') == 'true'){
+            target.setAttribute('aria-expanded','false');
+            target.className = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-combobox-picklist';
+        }else{
+            target.setAttribute('aria-expanded','true');
+            target.className = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-combobox-picklist slds-is-open';
+        }
+    },
+
+    outfocus : function(component, event, helper) {
+        var target = event.currentTarget.parentElement.parentElement;
+        target.setAttribute('aria-expanded','false');
+        target.className = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-combobox-picklist';
+    },
+
+
     optionClicked : function(component, event, helper) {
-        console.log('component ',component);
-        console.log('event ',event);
-        console.log('helper ',helper);
-        var target = event.target;
-        // var name = target.get('v.name');
-        console.log('option clicked ',target.attributes);
-        console.log('option clicked ',target.name);
-        
+        var target = event.currentTarget;
+        component.set('v.selectedLabel', target.getAttribute('data-label'));
+        component.set('v.selectedValue', target.getAttribute('data-value'));
     }
 })
