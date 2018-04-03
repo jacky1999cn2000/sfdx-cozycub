ead6c7519644a02289d821c072d094368f20161c
Jacky Zhao <liang.zhao83@gmail.com>
2018-04-02 22:56:29 -0700

    dropdown done

--- force-app/main/default/aura/UtilDropDownWithFilter/UtilDropDownWithFilterController.js
+++ force-app/main/default/aura/UtilDropDownWithFilter/UtilDropDownWithFilterController.js
@@ -1,4 +1,8 @@
 ({
+    init : function(component, event, helper) {
+        component.set('v.options', component.get('v.originalOptions'));
+    },
+
     toggle : function(component, event, helper) {
         var target = event.currentTarget;
         if(target.getAttribute('aria-expanded') == 'true'){
@@ -10,15 +14,25 @@
         }
     },
 
-    outfocus : function(component, event, helper) {
+    onblur : function(component, event, helper) {
         var target = event.currentTarget.parentElement.parentElement;
         target.setAttribute('aria-expanded','false');
         target.className = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-combobox-picklist';
     },
 
+    onkeydown : function(component, event, helper) {
+       var target = event.currentTarget;
+       var text = target.value;
+       var originalOptions = component.get('v.originalOptions');
+       var filteredOptions = originalOptions.filter(function(option) {
+           return option.label.indexOf(text) != -1;
+       });
+       component.set('v.options', filteredOptions);
+    },
 
     optionClicked : function(component, event, helper) {
         var target = event.currentTarget;
+        component.set('v.options', component.get('v.originalOptions'));
         component.set('v.selectedLabel', target.getAttribute('data-label'));
         component.set('v.selectedValue', target.getAttribute('data-value'));
     }
