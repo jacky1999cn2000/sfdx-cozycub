94e0d873ed314f76462ff4115470c7ebed78633f
Jacky Zhao <liang.zhao83@gmail.com>
2018-06-06 15:49:07 -0700

    players layout

--- sfdx-source/cozycub/main/default/aura/PlayerInfo/PlayerInfoController.js
+++ sfdx-source/cozycub/main/default/aura/PlayerInfo/PlayerInfoController.js
@@ -1,5 +1,16 @@
 ({
-    myAction : function(component, event, helper) {
+    init: function(component, event, helper) {
 
-    }
+        switch(component.get('v.emotion')){
+            case 'happy':
+                component.set('v.avatar', component.get('v.happy'));
+                break;
+            case 'sad':
+                component.set('v.avatar', component.get('v.sad'));
+                break;
+            default:
+                component.set('v.avatar', component.get('v.calm'));
+        }
+
+    },
 })
