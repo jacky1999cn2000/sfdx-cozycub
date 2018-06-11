09fabeada1746d2b9eb88c0e4e2aaaa2d8c36b24
Jacky Zhao <liang.zhao83@gmail.com>
2018-06-11 16:12:48 -0700

    add lodash and promise into component during loading

--- sfdx-source/cozycub/game/aura/GameSetupContainer/GameSetupContainerController.js
+++ sfdx-source/cozycub/game/aura/GameSetupContainer/GameSetupContainerController.js
@@ -1,5 +1,6 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+    init: function(component, event, helper) {
+        component.set('v.players', '[{"name":"Abbot J","money":"42","emotion":"calm","calm":"abottt","happy":"abotttttt","sad":"abott"},{"name":"Jack L","money":"42","emotion":"happy","calm":"jackttt","happy":"jackt","sad":"jackttttt"},{"name":"Kevin D","money":"42","emotion":"sad","calm":"kevintt","happy":"kevinttttt","sad":"kevintttttttt"},{"name":"Helen W","money":"42","emotion":"sad","calm":"helenttttttt","happy":"helent","sad":"helenttttt"},{"name":"Sam S","money":"42","emotion":"happy","calm":"samtt","happy":"samt","sad":"samtttt"},{"name":"Agata L","money":"42","emotion":"calm","calm":"agtttt","happy":"agtttttt","sad":"agtt"},{"name":"Grace E","money":"42","emotion":"happy","calm":"gawettt","happy":"gawetttttt","sad":"gawett"},{"name":"William P","money":"42","emotion":"happy","calm":"bedfttttt","happy":"bedf","sad":"bedfttt"},{"name":"Sharif O","money":"42","emotion":"sad","calm":"sfaaf","happy":"sfaafttt","sad":"sfaaftttttt"},{"name":"Olivia Z","money":"42","emotion":"sad","calm":"qett","happy":"qetttt","sad":"qe"}]');
+    },
 })
+
--- sfdx-source/cozycub/game/aura/Players/PlayersController.js
+++ sfdx-source/cozycub/game/aura/Players/PlayersController.js
@@ -1,5 +1,24 @@
 ({
-    myAction : function(component, event, helper) {
+    init: function(component, event, helper) {
+        var upperPlayers = [];
+        var lowerPlayers = [];
 
-    }
+        var players_string = component.get('v.players');
+        console.log('players_string ',players_string);
+
+        var players = JSON.parse(players_string);
+
+        console.log('players ',players);
+
+        for(var i = 0; i < players.length/2; i++){
+            upperPlayers.push(players[i]);
+        }
+
+        for(var i = players.length/2; i < players.length; i++){
+            lowerPlayers.push(players[i]);
+        }
+
+        component.set('v.upperPlayers', upperPlayers);
+        component.set('v.lowerPlayers', lowerPlayers);
+    },
 })
