71462c0e962fee56d5909769393fb26b819b16f2
Jacky Zhao <liang.zhao83@gmail.com>
2018-06-21 17:08:28 -0700

    remove unnecessary end game event status

--- sfdx-source/cozycub/game/aura/GameSetupContainer/GameSetupContainerController.js
+++ sfdx-source/cozycub/game/aura/GameSetupContainer/GameSetupContainerController.js
@@ -17,7 +17,7 @@
 
         // if there are still remaining round, then play the next round
         if(currentRound != round){
-            console.log('currentRound NOT equals to round');
+            console.log('next round...');
 
             setTimeout($A.getCallback(function(){
                 console.log('start timer...');
@@ -33,56 +33,7 @@
             }), 3000);
 
         }else{
-            console.log('currentRound equals to round');
-            console.log('firing game event...');
-            var gameEvent = component.getEvent("GameEvent");
-            if(gameEvent != null) {
-                gameEvent.setParam("status", 'end game');
-                // fire the event
-                gameEvent.fire(); 
-            }          
-        }
-
-    },
-
-    handleGameEvent : function(component, event, helper) {
-        console.log('handleGameEvent...');
-
-        var status = event.getParam("status");
-
-        if(status == 'new game'){
-            console.log('new game...');
-
-            var gameId = event.getParam("gameId");
-            var name = event.getParam("name");
-            var round = event.getParam("round");
-            var currentRound = event.getParam("currentRound");
-            var bet = event.getParam("bet");
-    
-            console.log('gameId: ', gameId);
-            console.log('name: ', name);
-            console.log('round: ', round);
-            console.log('currentRound: ', currentRound);
-            console.log('bet: ', bet);
-    
-            component.set('v.gameId', gameId);
-            component.set('v.name', name);
-            component.set('v.round', round);
-            component.set('v.currentRound', currentRound);
-            component.set('v.bet', bet);
-    
-            component.set('v.gameon', true);
-    
-            var action = component.get('c.playGame');
-            action.setParams({'gameId': gameId, 'name': name, 'round': round, 'currentRound': currentRound, 'bet': bet, 'players_string': null});
-            AuraPromise.serverSideCall(action, component).then(function(players) {
-                console.log('initial players '+players);
-                component.set('v.players', players);
-            }).catch(function(error) {
-                console.log('Error: ' + error);
-            });
-        }else{
-            console.log('end game...');
+            console.log('end game');
 
             component.set('v.gameId', '');
             component.set('v.name', '');
@@ -92,9 +43,44 @@
 
             setTimeout($A.getCallback(function(){
                 component.set('v.gameon', false);
-            }), 3000);
-
+            }), 3000);         
         }
+
+    },
+
+    handleGameEvent : function(component, event, helper) {
+        console.log('handleGameEvent...');
+
+        var status = event.getParam("status");
+
+        var gameId = event.getParam("gameId");
+        var name = event.getParam("name");
+        var round = event.getParam("round");
+        var currentRound = event.getParam("currentRound");
+        var bet = event.getParam("bet");
+
+        console.log('gameId: ', gameId);
+        console.log('name: ', name);
+        console.log('round: ', round);
+        console.log('currentRound: ', currentRound);
+        console.log('bet: ', bet);
+
+        component.set('v.gameId', gameId);
+        component.set('v.name', name);
+        component.set('v.round', round);
+        component.set('v.currentRound', currentRound);
+        component.set('v.bet', bet);
+
+        component.set('v.gameon', true);
+
+        var action = component.get('c.playGame');
+        action.setParams({'gameId': gameId, 'name': name, 'round': round, 'currentRound': currentRound, 'bet': bet, 'players_string': null});
+        AuraPromise.serverSideCall(action, component).then(function(players) {
+            console.log('initial players '+players);
+            component.set('v.players', players);
+        }).catch(function(error) {
+            console.log('Error: ' + error);
+        });
     }
 })
 
