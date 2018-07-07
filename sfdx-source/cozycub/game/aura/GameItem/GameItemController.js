6ca62c574322c1c093f3f42b61d371f7729ea9c8
Jacky Zhao <liang.zhao83@gmail.com>
2018-07-07 00:29:57 -0700

    add new components: gameitem, gamepanel

---
+++ sfdx-source/cozycub/game/aura/GameItem/GameItemController.js
@@ -0,0 +1,36 @@
+({
+    init : function(component, event, helper) {
+
+        var selected = component.get('v.selected');
+        var bet = component.get('v.bet');
+        var moneybags = component.get('v.moneybags');
+        var card = component.find('card');
+
+        console.log('moneybags ',moneybags);
+
+        if(bet <= 10){
+            moneybags = [1];
+        }else if(bet <= 20){
+            moneybags = [1,2];
+        }else if(bet <= 30){
+            moneybags = [1,2,3];
+        }else if(bet <= 40){
+            moneybags = [1,2,3,4];
+        }else{
+            moneybags = [1,2,3,4,5];
+        }
+
+        if(selected){
+            if(!$A.util.hasClass(card, "selected")){
+                $A.util.addClass(card, 'selected'); 
+            }           
+        }else{
+            if($A.util.hasClass(card, "selected")){
+                $A.util.removeClass(card, 'selected');
+            }
+        }
+
+        component.set('v.moneybags',moneybags);
+
+    }
+})
