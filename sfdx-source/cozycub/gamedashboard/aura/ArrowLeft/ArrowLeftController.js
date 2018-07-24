8b0eb860e522d5fb0d5fea28c0bbfc9e98f2bfbd
Jacky Zhao <liang.zhao83@gmail.com>
2018-07-24 15:11:26 -0700

    left & right arrows changing start/end Indexes

--- sfdx-source/cozycub/gamedashboard/aura/ArrowLeft/ArrowLeftController.js
+++ sfdx-source/cozycub/gamedashboard/aura/ArrowLeft/ArrowLeftController.js
@@ -1,5 +1,11 @@
 ({
-    myAction : function(component, event, helper) {
+    back : function(component, event, helper) {
+        console.log('back');
 
+        var startIndex = component.get('v.startIndex');
+        var endIndex = component.get('v.endIndex');
+
+        component.set('v.startIndex ', startIndex - 12);
+        component.set('v.endIndex ', endIndex - 12);
     }
 })
--- sfdx-source/cozycub/gamedashboard/aura/ArrowRight/ArrowRightController.js
+++ sfdx-source/cozycub/gamedashboard/aura/ArrowRight/ArrowRightController.js
@@ -1,11 +1,11 @@
 ({
     next : function(component, event, helper) {
         console.log('next');
+
         var startIndex = component.get('v.startIndex');
         var endIndex = component.get('v.endIndex');
-        console.log('startIndex ',startIndex);
-        console.log('endIndex ',endIndex);
-        component.set('v.startIndex',startIndex+12);
-        component.set('v.endIndex',endIndex+12);
+
+        component.set('v.startIndex ', startIndex + 12);
+        component.set('v.endIndex ', endIndex + 12);
     }
 })
